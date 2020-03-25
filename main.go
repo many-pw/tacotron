75fb12e9a2b80abe90b3fd3d8cb30b7e56671ea0
Andrew Arrow <andrew@bird.co>
2020-03-25 08:59:24 -0700

    split

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
 import "fmt"
+import "bufio"
+import "strings"
+import "io"
+import "os"
+
+var wavToWords = map[string]string{}
 
 func main() {
-	fmt.Println("vim-go")
+	readFileLines()
+	fmt.Println(len(wavToWords))
+}
+func readFileLines() {
+	f, err := os.OpenFile("metadata.csv", os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	rd := bufio.NewReader(f)
+	for {
+		line, err := rd.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+		  fmt.Println(err)
+			return
+		}
+		tokens := strings.Split(line, "|")
+		if len(tokens) >= 2 {
+			key := tokens[0]
+			txt := tokens[len(tokens)-1]
+			wavToWords[key] = txt
+		}
+	}
 }
