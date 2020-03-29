9eef9c4183d1d32e8127c5244e7e105d40d5c490
Andrew Arrow <andrew@many.pw>
2020-03-28 20:44:30 -0700

    good

--- melspectrogram.go
+++ melspectrogram.go
@@ -29,7 +29,7 @@ func stft(y []float32) string {
 	s = append(s, []float32{2.0})
 	s = append(s, []float32{3.0})
 	//s = append(s, []float32{4.0})
-	yy := pad(s, 2, "reflect")
+	yy := pad(s, 1, "reflect")
 	//yy := pad(y, int(math.Ceil(nfft/2.0)), "reflect")
 	fmt.Println(yy)
 	return "w"
@@ -39,13 +39,13 @@ func pad(y [][]float32, size int, padFlavor string) []float32 {
 	s := [][]float32{}
 	j := 0
 	if len(s)%4 == 0 {
-		j = len(y) - 1
+		j = len(y) - 2
 	} else if len(s)%4 == 1 {
 		j = len(y) - 1
 	} else if len(s)%4 == 2 {
-		j = len(y) - 2
-	} else {
 		j = len(y) - 1
+	} else {
+		j = len(y) - 2
 	}
 	direction := "down"
 	special := []float32{}
@@ -71,12 +71,11 @@ func pad(y [][]float32, size int, padFlavor string) []float32 {
 		if len(s) >= (size*2+len(y))-1 {
 			fmt.Println("a", special, "|", s)
 			if len(s)%4 == 0 {
-				//s = append(s, special)
 				s = append([][]float32{special}, s...)
 			} else if len(s)%4 == 1 {
-				s = append([][]float32{special}, s...)
+				s = append(s, special)
 			} else if len(s)%4 == 2 {
-				s = append([][]float32{special}, s...)
+				s = append(s, special)
 			} else {
 				s = append([][]float32{special}, s...)
 			}
