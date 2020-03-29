573d00d9fbe688a99a670c9ab719f850515297f9
Andrew Arrow <andrew@many.pw>
2020-03-29 13:47:52 -0700

    right

--- framing.go
+++ framing.go
@@ -10,16 +10,20 @@ type Frame struct {
 func findKids(f *Frame, level int) {
 	fmt.Print("[")
 	if len(f.List) == 0 {
-		fmt.Println(f.Data)
+		fmt.Print(f.Data)
 	} else {
 		for _, kid := range f.List {
 			findKids(kid, level+1)
 		}
 	}
+	if len(f.List) == 0 {
+		fmt.Print("]\n")
+	}
 }
 
 func (f *Frame) String() string {
 	findKids(f, 0)
+	fmt.Print("]\n")
 	return ""
 }
 
@@ -42,14 +46,5 @@ func frame(y []float32, frameLength, hopLength int) *Frame {
 	wrapper.List = append(wrapper.List, &f2)
 
 	froot.List = append(froot.List, &wrapper)
-	/*
-		aa1 := [][]float32{a1}
-		aa2 := [][]float32{a2}
-
-		aaa1 := [][][]float32{aa1, aa2}
-
-		aaaa1 := [][][][]float32{aaa1}
-		fmt.Println(aaaa1)
-	*/
 	return &froot
 }
