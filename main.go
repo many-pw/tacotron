084dd3ab98afab6659e0c2e9856ba04d8b7ed733
Andrew Arrow <andrew@many.pw>
2020-04-04 21:19:30 -0700

    back

--- main.go
+++ main.go
@@ -1,26 +1,21 @@
 package main
 
-import wave "github.com/zenwerk/go-wave"
 import "os"
-import "math"
 import "fmt"
+import "github.com/many-pw/tacotron/wav"
 
 func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("enter file")
 		return
 	}
-	waveReader, _ := wave.NewReader(os.Args[1])
-	bitsPerSample := 16
-	for {
-		framesPerBuffer, _ := waveReader.ReadRawSample()
-
-		for i, f := range framesPerBuffer {
-
-			val := float64(f) / math.Pow(2, float64(bitsPerSample))
-
-			fmt.Println(val, i, f,
-				waveReader.NumSamples, waveReader.ReadSampleNum)
-		}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
 	}
+	reader := wav.NewReader(file)
+	f, _ := reader.Format()
+	fmt.Println("sr", f.SampleRate, "channels", f.NumChannels)
+	fmt.Println("byteRate", f.ByteRate, "BlockAlign", f.BlockAlign)
+	fmt.Println("BitsPerSample", f.BitsPerSample)
 }
